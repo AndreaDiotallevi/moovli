7e1697a40236f937e653b3bafcc62458e87a681f
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-02-07 14:30:20 +0000

    feat: add onclick functino for lat & lng

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 import { Map, InfoWindow, GoogleApiWrapper } from 'google-maps-react';
 
 class MapContainer extends Component {
+  onClick(t, map, coord) {
+    const { latLng } = coord;
+    const lat = latLng.lat();
+    const lng = latLng.lng();
+    console.log(lat, lng)
+  }
+
   render() {
     return (
       <div>
         <Map
           google={this.props.google}
           zoom={4}
+          onClick={this.onClick}
           initialCenter={{
             lat: 51.509865,
             lng: -0.118092,

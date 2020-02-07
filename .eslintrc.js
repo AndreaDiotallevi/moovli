ed8f89206555a5fd432da8eaec6254a0d96c745c
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-02-07 14:26:58 +0000

    fix: amend code as per eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,8 @@ module.exports = {
     "extends": ['airbnb'],
     "rules": {
         'react/jsx-filename-extension':'off',
-        'no-undef':'off'
+        'no-undef':'off',
+        'react/destructuring-assignment':'off',
+        'react/prop-types':'off'
     }
 };
--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,26 +1,25 @@
 import React, { Component } from 'react';
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import { Map, InfoWindow, GoogleApiWrapper } from 'google-maps-react';
 
 class MapContainer extends Component {
   render() {
     return (
-     <div>
-       < Map
-         google={this.props.google}
-         zoom={4}
-         initialCenter={{
-         lat: 51.509865,
-         lng: -0.118092
-         }}
-       >
-       <InfoWindow onClose={this.onInfoWindowClose}>
-       </InfoWindow>
-       </Map>
-    </div>
+      <div>
+        <Map
+          google={this.props.google}
+          zoom={4}
+          initialCenter={{
+            lat: 51.509865,
+            lng: -0.118092,
+          }}
+        >
+          <InfoWindow onClose={this.onInfoWindowClose} />
+        </Map>
+      </div>
     );
   }
 }
 
 export default GoogleApiWrapper({
-  apiKey:"AIzaSyASYegQUtea-hnik-mLh749REFM5DVS_iA"
-})(MapContainer)
+  apiKey: 'AIzaSyASYegQUtea-hnik-mLh749REFM5DVS_iA',
+})(MapContainer);

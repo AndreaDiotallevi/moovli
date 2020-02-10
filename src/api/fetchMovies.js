7c9d5a51d5ff82331dc23b4652adf11ce67db21c
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-02-10 12:13:58 +0000

    fix: eslint convention

--- src/api/fetchMovies.js
+++ src/api/fetchMovies.js
@@ -1,18 +1,16 @@
-import fetchMovieData from './fetchMovieData'
-import fetchMoviesTitles from './fetchMoviesTitles'
+import fetchMovieData from './fetchMovieData';
+import fetchMoviesTitles from './fetchMoviesTitles';
 
 const fetchMovies = (country) => {
-  let moviesTitles = fetchMoviesTitles(country);
-  return moviesTitles.map(async (title) => {
-    return await fetchMovieData(title).then(response => {
-      if (response.results.length > 0) {
-        return {
-          id: response.results[0].id,
-          title: response.results[0].title
-        }
-      }
-    })
-  })
+  const moviesTitles = fetchMoviesTitles(country);
+  return moviesTitles.map(async (title) => await fetchMovieData(title).then((response) => {
+    if (response.results.length > 0) {
+      return {
+        id: response.results[0].id,
+        title: response.results[0].title,
+      };
+    }
+  }));
 };
 
 export default fetchMovies;
--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,24 +1,25 @@
 import React from 'react';
 
-const Movies = (props) => {
-  return (
-    <div data-test='movies'>
-      <h1 data-test='movies-country-message'>
-        You have choosen {props.country}!
-      </h1>
-      <div data-test='movies-container'>
-        <ul>
-          {props.movies.map(movie => 
-            <li key={movie.id}>
-              <h3>
-                {movie.title}
-              </h3>
-            </li>
-          )}
-        </ul>
-      </div>
+const Movies = (props) => (
+  <div data-test="movies">
+    <h1 data-test="movies-country-message">
+        You have choosen
+      {' '}
+      {props.country}
+!
+    </h1>
+    <div data-test="movies-container">
+      <ul>
+        {props.movies.map((movie) => (
+          <li key={movie.id}>
+            <h3>
+              {movie.title}
+            </h3>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
- 
+  </div>
+);
+
 export default Movies;

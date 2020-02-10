542cef16a7a6b8852a041574984dec466bf232c8
Daniel <demartinezgatell@gmail.com>
2020-02-10 15:13:49 +0000

    feat: back button visible

--- src/App.js
+++ src/App.js
@@ -26,11 +26,16 @@ class App extends Component {
             })
   }
 
-  render() { 
+  handleBackToHome = () => {
+    const noCountry = ''
+    this.setState({noCountry})
+  }
+
+  render() {
     return (
       <div className='App'>
         {this.state.movies.length === 0 && <Home onCountryChoice={this.handleCountryChoice}/>}
-        {this.state.movies.length !== 0 && <Movies movies={this.state.movies} country={this.state.country}/>}
+        {this.state.movies.length !== 0 && <Movies movies={this.state.movies} country={this.state.country} onBacktoHome={this.handleBackToHome}/>}
       </div>
     );
   }

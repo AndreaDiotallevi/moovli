35734cf7d931959284c32c0c2961b23e8ab58cfa
AndreaDiotallevi <andrea.diotallevi@outlook.com>
2020-02-11 23:28:09 +0000

    fix: remove google geocoder api and use locationiq api

--- src/api/fetchCountry.js
+++ src/api/fetchCountry.js
@@ -1,9 +1,9 @@
 const fetchCountry = (t, map, coord) => {
-  const API_KEY = process.env.REACT_APP_GEO_CODE_API;
+  const API_KEY = process.env.REACT_APP_LOCATION_API;
   const { latLng } = coord;
   const lat = latLng.lat();
   const lng = latLng.lng();
-  return fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${API_KEY}`).then((response) => response.json());
+  return fetch(`https://us1.locationiq.com/v1/reverse.php?key=5850fb1716ee9f&lat=${lat}&lon=${lng}&format=json`).then((response) => response.json());
 };
 
 export default fetchCountry;

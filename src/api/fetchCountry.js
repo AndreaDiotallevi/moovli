f954bd695f05f94bf11cc925a46b3af99d32734e
AndreaDiotallevi <andrea.diotallevi@outlook.com>
2020-02-11 23:33:43 +0000

    fix: hide locationiq api key

--- src/api/fetchCountry.js
+++ src/api/fetchCountry.js
@@ -3,7 +3,7 @@ const fetchCountry = (t, map, coord) => {
   const { latLng } = coord;
   const lat = latLng.lat();
   const lng = latLng.lng();
-  return fetch(`https://us1.locationiq.com/v1/reverse.php?key=5850fb1716ee9f&lat=${lat}&lon=${lng}&format=json`).then((response) => response.json());
+  return fetch(`https://us1.locationiq.com/v1/reverse.php?key=${API_KEY}&lat=${lat}&lon=${lng}&format=json`).then((response) => response.json());
 };
 
 export default fetchCountry;

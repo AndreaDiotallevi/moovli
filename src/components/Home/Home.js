b09096158f92386788e0990d11ba161b735add64
root <hish.b92@gmail.com>
2020-02-13 16:29:45 +0000

    Fix: home page nav bar

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,13 +2,17 @@ import React from 'react';
 import MapContainer from '../MapContainer/MapContainer';
 
 const Home = (props) => (
-  <div data-test="app">
-    <h1 data-test="app-title">Moovli</h1>
-    <MapContainer
-      onCountryChoice={props.onCountryChoice}
-      infoWindowVisibile={props.infoWindowVisibile}
-      onClickCoordLatLng ={props.onClickCoordLatLng}
-    />
+  <div className='home-container' data-test="app">
+    <div className='home-header'>
+      <h1 className='home-header-title' data-test="app-title">Moovli - Click on a country and find the best movies from that country!</h1>
+    </div>
+    <div className='map-container'>
+      <MapContainer
+        onCountryChoice={props.onCountryChoice}
+        infoWindowVisibile={props.infoWindowVisibile}
+        onClickCoordLatLng ={props.onClickCoordLatLng}
+      />
+    </div>
   </div>
 );
 

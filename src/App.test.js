a0b43e27fea7ae49999c3b5871128b314b2e9dab
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-02-13 23:42:53 +0000

    style: linting app file  & test

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
 import { shallow } from 'enzyme';
+import App from './App';
 
-const setUp = (Component, props = {}) => {
+const setUp = (Component = {}) => {
   const component = shallow(<Component />);
   return component;
-}
+};
 
 describe('App', () => {
-
   it('should render without errors', () => {
     const wrapper = setUp(App);
     expect(wrapper).toHaveLength(1);
-  })
-  
-})
+  });
+});

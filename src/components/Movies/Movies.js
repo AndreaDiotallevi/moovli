e874e9924f4affb22f3b1be5da18873055f4a20f
root <hish.b92@gmail.com>
2020-02-13 15:20:49 +0000

    Fix: rating

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -65,10 +65,10 @@ class Movies extends Component {
                 </div>
                 <div className='average-genres-container'>
                   <p className='movie-vote-average' data-test={`movie-vote-average-${movie.id}`}>
-                    <span className='average-rating-title'>Average Rating: </span>{movie.voteAverage}
+                    <span className='average-rating-title'>IMDB Rating: </span>{movie.ratings[0].Value.split('/10')[0]}
                   </p>
                   <p className='movie-genre-list' data-test={`movie-genre-list-${movie.id}`}>
-                    Genres: {movie.genreList.join(", ")}
+                    <span className='average-rating-title'>Genres: </span>{movie.genreList.join(", ")}
                   </p>
                 </div>
               </div>

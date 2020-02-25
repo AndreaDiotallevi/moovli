f97b51a5b240f81c30083c6a4d6640ec128bfbee
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-02-25 12:49:59 +0000

    refactor: fetchMovie.js and MapContainer.js

--- src/components/MapContainer/MapContainer.js
+++ src/components/MapContainer/MapContainer.js
@@ -2,34 +2,28 @@ import React, { Component } from 'react';
 import { Map, InfoWindow, GoogleApiWrapper } from 'google-maps-react';
 
 class MapContainer extends Component {
-  onCountryChoice = (t, map, coord) => {
-    this.props.onCountryChoice(t, map, coord);
-  }
-
   render() {
     return (
-      <div data-test='map-container'>
         <Map
           google={this.props.google}
           zoom={4}
-          onClick={this.onCountryChoice}
+          onClick={this.props.onCountryChoice}
           initialCenter={{
             lat: 51.509865,
             lng: -0.118092,
             }}
         >
-        <InfoWindow
-          position = {{
-            lat: (this.props.onClickCoordLatLng[0]),
-            lng: (this.props.onClickCoordLatLng[1]),
-          }}
-          visible={this.props.infoWindowVisibile}>
-            <div>
-              <h1>There are no movies for this country</h1>
-            </div>
-        </InfoWindow>
+          <InfoWindow
+            position = {{
+              lat: (this.props.onClickCoordLatLng[0]),
+              lng: (this.props.onClickCoordLatLng[1]),
+            }}
+            visible={this.props.infoWindowVisibile}>
+              <div>
+                <h4>There are no movies for this country</h4>
+              </div>
+          </InfoWindow>
         </Map>
-      </div>
     );
   }
 }
